5c4d1d3b78310e1f77b18b67679e4bf5b49798ad
Peter Njiru <peternjirum@gmail.com>
2023-07-26 13:38:15 -0400

    completes bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate", 2, "M","red","calm","True");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 12, "M","black","agressive","False");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 5, "F","white","calm","True");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 7, "M","grey","calm","True");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Melissa", 6, "F","white","playful","False");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 9, "M","black","calm","True");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 1, "M","beige","agressive","True");
+INSERT into bears (name,age,sex,color,temperament,alive) VALUES (NULL, 5, "F","ash","calm","False");
--- lib/testing/select_test.py
+++ lib/testing/select_test.py
@@ -22,25 +22,28 @@ insert_file = open("lib/seed.sql")
 insert_as_string = insert_file.read()
 cursor.executescript(insert_as_string)
 
+
 class TestSelectAllFemaleBearsReturnNameAndAge:
     '''select_all_female_bears_return_name_and_age in sql_queries.py'''
 
     def test_selects_females_and_returns_name_and_age(self):
         '''selects all of the female bears and returns their name and age.'''
         result = cursor.execute(select_all_female_bears_return_name_and_age)
-        assert(result.fetchall() == [
+        assert (result.fetchall() == [
             ("Tabitha", 6),
             ("Melissa", 13),
             ("Wendy", 6)
         ])
+
 
 class TestSelectAllBearsNamesAndOrdersInAlphabeticalOrder:
     '''select_all_bears_names_and_orders_in_alphabetical_order in sql_queries.py'''
 
     def test_selects_all_bears_names_and_orders_alphabetically(self):
         '''selects all of the bears names and orders them in alphabetical order.'''
-        result = cursor.execute(select_all_bears_names_and_orders_in_alphabetical_order)
-        assert(result.fetchall() == [
+        result = cursor.execute(
+            select_all_bears_names_and_orders_in_alphabetical_order)
+        assert (result.fetchall() == [
             (None,),
             ("Grinch",),
             ("Melissa",),
@@ -51,13 +54,15 @@ class TestSelectAllBearsNamesAndOrdersInAlphabeticalOrder:
             ("Wendy",)
         ])
 
+
 class TestSelectAllBearsNamesAndAgesThatAreAliveAndOrderYoungestToOldest:
     '''select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest in sql_queries.py'''
 
     def test_selects_all_bears_names_and_ages_that_are_alive_and_orders_youngest_to_oldest(self):
         '''selects all of the bears names and ages that are alive and order them from youngest to oldest.'''
-        result = cursor.execute(select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest)
-        assert(result.fetchall() == [
+        result = cursor.execute(
+            select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest)
+        assert (result.fetchall() == [
             ("Grinch", 2),
             ("Tabitha", 6),
             ("Wendy", 6),
@@ -65,15 +70,17 @@ class TestSelectAllBearsNamesAndAgesThatAreAliveAndOrderYoungestToOldest:
             ("Melissa", 13),
         ])
 
+
 class TestSelectOldestBearAndReturnNameAndAge:
     '''select_oldest_bear_and_returns_name_and_age in sql_queries.py'''
 
     def test_selects_oldest_bear_and_returns_name_and_age(self):
         '''selects the oldest bear and returns its name and age.'''
         result = cursor.execute(select_oldest_bear_and_returns_name_and_age)
-        assert(result.fetchall() == [
+        assert (result.fetchall() == [
             ("Mr. Chocolate", 20,),
         ])
+
 
 class TestSelectYoungestBearAndReturnNameAndAge:
     '''select_youngest_bear_and_returns_name_and_age in sql_queries.py'''
@@ -81,6 +88,6 @@ class TestSelectYoungestBearAndReturnNameAndAge:
     def test_selects_youngest_bear_and_returns_name_and_age(self):
         '''selects the youngest bear and returns its name and age.'''
         result = cursor.execute(select_youngest_bear_and_returns_name_and_age)
-        assert(result.fetchall() == [
+        assert (result.fetchall() == [
             ("Grinch", 2,),
         ])
