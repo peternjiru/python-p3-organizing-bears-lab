f1d8131b3e665c47e5f16e63cd026378ce1580d6
Peter Njiru <peternjirum@gmail.com>
2023-07-26 13:44:55 -0400

    completes bears lab

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -7,7 +7,7 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT name,age FROM bears where alive = True ORDER BY age;
+    SELECT name,age FROM bears where alive ORDER BY age;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
